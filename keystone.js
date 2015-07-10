57228d325a2d22b7110e6e32ee576f28e8e5bc12
Samu Mikkonen <samu.mikkonen@gmail.com>
2015-07-10 23:07:39 +0300

    initial keystonejs commit

---
+++ keystone.js
@@ -0,0 +1,67 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').load();
+
+// Require keystone
+var keystone = require('keystone');
+var cons = require('consolidate');
+var nunjucks = require('nunjucks');
+
+// Initialise Keystone with your project's configuration.
+// See http://keystonejs.com/guide/config for available options
+// and documentation.
+
+keystone.init({
+
+	'name': 'Site patterns',
+	'brand': 'Site patterns',
+	
+	'sass': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'html',
+	'custom engine': cons.nunjucks,
+	
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+	'cookie secret': '[3B<CiVV(;Orw0$6g6;|*N%_LZf)4Z/L%*{qdS^410k`9!42?<(W7|2-=OV=(p_/'
+
+});
+
+// Load your project's Models
+
+keystone.import('models');
+
+// Setup common locals for your templates. The following are required for the
+// bundled templates and layouts. Any runtime locals (that should be set uniquely
+// for each request) should be added to ./routes/middleware.js
+
+keystone.set('locals', {
+	_: require('underscore'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable
+});
+
+// Load your project's Routes
+
+keystone.set('routes', require('./routes'));
+
+// Setup common locals for your emails. The following are required by Keystone's
+// default email templates, you may remove them if you're using your own.
+
+// Configure the navigation bar in Keystone's Admin UI
+
+keystone.set('nav', {
+	'posts': ['posts', 'post-categories'],
+	'galleries': 'galleries',
+	'enquiries': 'enquiries',
+	'users': 'users'
+});
+
+// Start Keystone to connect to your database and initialise the web server
+
+keystone.start();
