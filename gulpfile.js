519d0b5ec95c9a87e39152653ec388b405c734a1
Samu Mikkonen <samu.mikkonen@gmail.com>
2015-07-12 19:49:45 +0300

    added libsass and pleeease to gulp and center aligned the jumbotron text

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pleeease = require('gulp-pleeease');
 var jshint = require('gulp-jshint');
 var jshintReporter = require('jshint-stylish');
 var watch = require('gulp-watch');
+
 
 /*
  * Create variables for our project paths so we can change in one place
@@ -9,6 +12,26 @@ var watch = require('gulp-watch');
 var paths = {
 	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
 };
+
+var sassPaths = {
+	'src':['./public/styles/**/*.scss']
+};
+
+
+// Sass
+
+gulp.task('sass', function () {
+    gulp.src(sassPaths.src)
+        .pipe(sass({errLogToConsole: true})) // Keep running gulp even though occurred compile error
+        .pipe(pleeease({
+            minifier: false,
+            autoprefixer: {
+                browsers: ['last 2 versions']
+
+            }
+        }))
+        .pipe(gulp.dest('./public/styles'))
+});
 
 
 // gulp lint
@@ -25,4 +48,12 @@ gulp.task('watch:lint', function () {
 		.pipe(watch())
 		.pipe(jshint())
 		.pipe(jshint.reporter(jshintReporter));
+});
+
+gulp.task('default', function() {
+    gulp.watch(sassPaths.src,['sass']);
+    gulp.src(paths.src)
+  		.pipe(watch())
+  		.pipe(jshint())
+  		.pipe(jshint.reporter(jshintReporter));
 });
