14c46752d4b0303cb09c5a706d23afedc61057c8
Samu Mikkonen <samu.mikkonen@gmail.com>
2015-08-05 16:29:40 +0300

    added pattern model

--- models/Pattern.js
+++ models/Pattern.js
@@ -2,25 +2,25 @@ var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
 /**
- * Layout Model
+ * Pattern Model
  * ==========
  */
 
- var Layout = new keystone.List('Layout', {
+ var Pattern = new keystone.List('Pattern', {
   map: { name: 'title' },
   autokey: { path: 'key', from: 'title', unique: true }
  });
 
- Layout.add({
+ Pattern.add({
  	title: { type: String, required: true },
-  sites: { type: Types.Relationship, ref: 'Site' },
  	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
  	author: { type: Types.Relationship, ref: 'User', index: true },
  	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+  code: { type: Types.Code, height: 400, language: html }
  	image: { type: Types.CloudinaryImage },
   description: { type: Types.Html, wysiwyg: true, height: 150, label: 'Description' },
   context: { type: Types.Html, wysiwyg: true, height: 150, label: 'Context of use' }
  });
 
 
-Layout.register();
+Pattern.register();
