5d2943feaf020c0980d0d7c12fac013925aeff2c
Samu Mikkonen <samu.mikkonen@gmail.com>
2015-08-12 18:24:24 +0300

    Added archiving code to download test

--- routes/util/download.js
+++ routes/util/download.js
@@ -1,5 +1,33 @@
+var archiver = require('archiver');
+var p = require('path');
+var fs = require('fs');
 
 exports = module.exports = function(req, res){
-    var file = __dirname + '/index.html';
-    res.download(file); // Set disposition and send it.
+
+    var archive = archiver('zip');
+
+    archive.on('error', function(err) {
+      res.status(500).send({error: err.message});
+    });
+
+    //on stream closed we can end the request
+    res.on('close', function() {
+      console.log('Archive wrote %d bytes', archive.pointer());
+      return res.status(200).send('OK').end();
+    });
+
+    //set the archive name
+    res.attachment('archive-name.zip');
+
+    //this is the streaming magic
+    archive.pipe(res);
+
+    var files = [__dirname + '/index.html', __dirname + '/hero.html'];
+
+    for(var i in files) {
+      archive.append(fs.createReadStream(files[i]), { name: p.basename(files[i]) });
+    }
+
+    archive.finalize();
+
 };
