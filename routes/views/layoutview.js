89cc045daa55b22ee53811a90bbe92fbf5223e4b
Samu Mikkonen <samu.mikkonen@gmail.com>
2015-09-03 11:31:11 +0300

    Refactored to remove the deepPopulate approach and instead use pattern model

--- routes/views/layoutview.js
+++ routes/views/layoutview.js
@@ -51,18 +51,55 @@ exports = module.exports = function(req, res) {
 	// Load all the patterns
 	view.on('init', function(next) {
 
-		var q = keystone.list('Layout').model.find();
 
-		q.where('state', 'published').populate('sites patterns').deepPopulate('patterns.patternCategories');
+		/*var q = keystone.list('Layout').model.find();*/
 
-		if (locals.filters.site) {
+		/*var options = {
+		  patternCategories: {
+		    options: {
+		      sort: '-name'
+		    }
+		  }
+		}*/
+
+		/*q.where('state', 'published').populate('sites patterns').deepPopulate('patterns.patternCategories');*/
+
+		/*if (locals.filters.site) {
+			console.log(locals.filters.site);
 			q.where('sites').in([locals.filters.site]);
-		}
+		}*/
 
-		q.exec(function(err, result) {
-			locals.data.patterns = result;
-			next(err);
+		/*q.where('key', 'front-page-big-images');*/
+
+		/*var site = locals.filters.siteName;
+		var query = {};
+		query['sites.key.' + site] = {$exists: true};
+		console.log(query);
+		q.find(query);*/
+
+		var q = keystone.list('Pattern').model.find();
+
+		// Get the _ids of the users of the selected category
+		keystone.list('PatternCategory').model.find({name:'Navigation'}, {_id: 1}, function(err, categories) {
+
+		    // Map the user docs into an array of just the _ids
+		    var ids = categories.map(function(category) { return category._id; });
+				/*console.log(ids);*/
+
+		    // Get the patterns whose patternCategories is in that set of ids
+		    keystone.list('Pattern').model.find({patternCategories: {$in: ids}}).exec(function(err, patterns) {
+						var ids = patterns.map(function(pattern) { return pattern._id; });
+					  q.find({'_id': {$in: ids}});
+
+						q.exec(function(err, result) {
+							locals.data.patterns = result;
+							next(err);
+						});
+
+		    });
+
 		});
+
 
 	});
 
