b71eecd9a3b9cb3211e6f81b62e0b01a0b03cd9f
Samu Mikkonen <samu.mikkonen@gmail.com>
2015-09-09 12:08:12 +0300

    Implemented quick jquery viewport resizing

--- public/js/app.js
+++ public/js/app.js
@@ -118,7 +118,7 @@ $(function() {
   }, '#code .card-container');
 
 
-     // Switch the accordion icons state
+    //  Switch the accordion icons state
      function toggleChevron(e) {
       e.stopPropagation();
       $(e.target).prev('.card-header').find('i.fa').toggleClass('fa-caret-down fa-caret-right');
@@ -126,6 +126,36 @@ $(function() {
      }
 
     $('.card-collapse').on('hidden.bs.collapse shown.bs.collapse', toggleChevron);
+
+
+  /*Change the view port container size by clicking the icons TODO: Switch to using iframe */
+
+  var columns = $('.col-sm-1, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-sm-10, .col-sm-11, .col-sm-12, .col-md-1, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-md-10, .col-md-11, .col-md-12, .col-lg-1, .col-lg-2, .col-lg-3, .col-lg-4, .col-lg-5, .col-lg-6, .col-lg-7, .col-lg-8, .col-lg-9, .col-lg-10, .col-lg-11, .col-lg-12, .col-xl-1, .col-xl-2, .col-xl-3, .col-xl-4, .col-xl-5, .col-xl-6, .col-xl-7, .col-xl-8, .col-xl-9, .col-xl-10, .col-xl-11, .col-xl-12');
+
+  $('#viewport-mobile').click(function(){
+      $('.container-resizable').removeClass('xl lg md xs-sm').addClass('sm');
+      columns.removeClass('pull-none');
+  });
+
+  $('#viewport-tablet').click(function(){
+      $('.container-resizable').removeClass('xl lg sm xs-sm').addClass('md');
+      columns.removeClass('pull-none');
+  });
+
+  $('#viewport-laptop').click(function(){
+      $('.container-resizable').removeClass('xl md sm xs-sm').addClass('lg');
+      columns.removeClass('pull-none');
+  });
+
+  $('#viewport-desktop').click(function(){
+      $('.container-resizable').removeClass('lg md sm xs-sm').addClass('xl');
+      columns.removeClass('pull-none');
+  });
+
+  $('#viewport-mobile-small').click(function(){
+      $('.container-resizable').removeClass('xl lg md sm').addClass('xs-sm');
+      columns.addClass('pull-none');
+  });
 
 
   /*Post the layout to server*/
